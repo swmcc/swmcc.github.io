ec922dd4b479108f22822e1e5e66324995903121
Stephen McCullough <me@swm.cc>
2014-03-31 11:44:33 +0100

    Made sure I left the tests in a passing state

--- test/site_test.js
+++ test/site_test.js
@@ -17,7 +17,17 @@ describe('test the config setup', function(){
 });
 
 describe('test that config.yml is set up correctly', function(){
- it('Check to see if the about links exists', function(){
- });
+  it('Check to see if the about links exists', function(){
+  
+      var buffer = fs.readFileSync(files['config']),
+      file = buffer.toString('utf8');
+    
+      it(files['config'], function() {
+        assert.doesNotThrow(function() { jsyaml.load(frontmatter); });
+      });
+
+//      assert.ok('name' in file, '_config contains a name parameter'); 
+
+  });
 });
 
