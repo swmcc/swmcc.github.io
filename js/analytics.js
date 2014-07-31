4e211f10b50677608165c6424912710cf70ff53f
Stephen McCullough <me@swm.cc>
2014-07-31 17:41:13 +0100

    Test that analytics are now being tracked via an event catch

---
+++ js/analytics.js
@@ -0,0 +1,17 @@
+logging = true;
+
+$('a').click(function(){
+  if (typeof $(this).data('ga') === "undefined") {
+    if (logging == true) {
+      // Take attributes of the element and fire through instead
+      // of logging
+      console.warn('no data-ga for clicked element: ', $(this));
+    }
+  } else {
+    var gaArray = $(this).data('ga').replace(/\s/g,'').split(',');
+    _gaq.push(['_trackEvent', gaArray[0], gaArray[1], gaArray[2]]);
+    if (logging == true) {
+      console.log('fired GA: ', ['_trackEvent', gaArray[0], gaArray[1], gaArray[2]]);
+    }
+  }
+});
